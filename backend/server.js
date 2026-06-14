7890835fd2deb869fd0f088fd44e62b6325ad1ec
Lychnis <happa641@gmail.com>
2026-06-14 18:42:40 +0900

    駅のIDを取得

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,31 @@
-import express from "express";
-import cors from "cors";
+// 駅名から駅IDを取得するヘルパー
+const stationNames = {
+  home: "宇治",
+  univ: "茨木",
+};
 
-const app = express();
-const port = 3000;
+const apiUrl = "https://navitime-transport.p.rapidapi.com/transport_node";
+const apiKey = process.env.RAPIDAPI_KEY || "55cc7e59a7msh9027c9e71464916p19052cjsn5ec33c65833a";
 
-app.use(cors());
+const requestOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": apiKey,
+    "x-rapidapi-host": "navitime-transport.p.rapidapi.com",
+    "Content-Type": "application/json",
+  },
+};
 
-app.get("/", (_, res) => {
-  res.send("Hello from server.js!!!");
-});
+async function fetchStationId(stationName) {
+  const response = await fetch(`${apiUrl}?word=${encodeURIComponent(stationName)}`, requestOptions);
+  const data = await response.json();
+  return data.items?.[0]?.id ?? null;
+}
 
-app.listen(port, () => {
-  console.log(`Server started. port: ${port}`);
-});
+const homeStationId = await fetchStationId(stationNames.home);
+const univStationId = await fetchStationId(stationNames.univ);
+
+console.log({ homeStationId, univStationId });
+
+export { homeStationId, univStationId, stationNames };
+
