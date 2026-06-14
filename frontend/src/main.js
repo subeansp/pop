eb01a7fb82425b053bf3e42daf4f61c3b39a6cd2
2525-drop <kairyu0816@gmail.com>
2026-06-14 15:13:48 +0900

    リンク開いてすぐの画面

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,40 @@
-const div = document.getElementById("app");
-const h1 = document.createElement("h1");
 
-const res = await fetch("http://localhost:3000");
-const content = await res.text();
+const app = document.getElementById("app");
 
-h1.textContent = content;
-div.appendChild(h1);
+// カード作成
+const card = document.createElement("div");
+card.className = "card";
+
+// タイトル
+const title = document.createElement("h1");
+title.textContent = "今からDoする？";
+
+// ボタンの箱
+const group = document.createElement("div");
+group.className = "button-group";
+
+// ボタン① 家
+const homeBtn = document.createElement("button");
+homeBtn.textContent = "家に帰る";
+
+// ボタン② 大学
+const uniBtn = document.createElement("button");
+uniBtn.textContent = "大学に行く";
+
+// クリックイベント（仮）
+homeBtn.onclick = () => {
+  alert("家に帰るを選択");
+};
+
+uniBtn.onclick = () => {
+  alert("大学に行くを選択");
+};
+
+// まとめる
+group.appendChild(homeBtn);
+group.appendChild(uniBtn);
+
+card.appendChild(title);
+card.appendChild(group);
+
+app.appendChild(card);
